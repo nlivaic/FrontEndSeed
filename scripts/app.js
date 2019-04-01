f1b7b4a2ca66beeda6cb88630874994941beab51
nlivaic <nlivaic@gmail.com>
2019-04-01 22:10:51 +0200

    chore: Babel v7, Bootstrap, jQuery.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 import '../styles/main.css';
-import { printMe } from './messages';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'jquery/dist/jquery.min.js'
+import * as messages from './messages';
 
-printMe('Hello world!');
+
+messages.printMe('Hello world!');
+messages.printYou('Hello world!');
+messages.printThree('Hello world!');
+
+let inputTask = document.querySelector('#inputTask'); 
+
+
 
 module.hot.accept();
--- scripts/messages.js
+++ scripts/messages.js
@@ -1,3 +1,12 @@
 export function printMe(txt) {
     console.log(`This is a message in ES6: ${txt}`);
 }
+
+export function printYou(txt) {
+    console.log(`This is a YOU message in ES6: ${txt}`);
+}
+
+export function printThree(txt) {
+    console.log(`This is a YOU message in ES6: ${txt}`);
+
+}
